0bde29068defd6f1e40fd4a13a70222da28a7de3
Sakshi Bhatkar <bhatkarsakshi09@gmail.com>
2024-09-24 11:25:31 +0530

    first commit: NavBar with dark and light theme

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import Logo from "/src/assets/logo.png"
+import { HiMenuAlt1, HiMenuAlt3 } from "react-icons/hi";
+import DarkMode from "./DarkMode";
+
+const NavLinks = [
+    {
+        id: 1,
+        name: "Home",
+        link: "#"
+    },
+    {
+        id: 2,
+        name: "Products",
+        link: "#"
+    },
+    {
+        id: 3,
+        name: "Pricing",
+        link: "#"
+    },
+    {
+        id: 4,
+        name: "Contact",
+        link: "#"
+    }
+]
+const Navbar = () => {
+    const [showMenu, setShowmenu] = React.useState(false)
+    const toggleMenu = () => setShowmenu(!showMenu);
+    return (
+        <div className=" relative z-[9999] text-black dark:text-white duration-480">
+            <div>
+                <div className="container py-2 md:py-0">
+                    <div className="flex justify-between items-center">
+                        {/*logo */}
+                        <div className="flex items-center gap-3">
+                            <img src={Logo} alt="" className="h-16" />
+                            <p className="text-3xl">
+                                VR <span className="font-bold">World</span>
+                            </p>
+                        </div>
+                        {/*Menu */}
+
+                        <nav className="hidden md:block">
+                            <ul className="flex items-center gap-8">
+                                {
+                                    NavLinks.map(({ id, name, link }) => {
+                                        return (
+                                            <li key={id} className="py-4">
+                                                <a href={link} className="text-xl font-semibold hover:text-primary py-2 
+                                              hover:border-b-2
+                                              hover:border-secondary 
+                                              transition-colors duration-500" >{name}</a>
+                                            </li>
+                                        );
+                                    })
+                                }
+                               {/*  DarkMode */}
+                               <DarkMode/>
+                            </ul>
+                        </nav>
+
+                        {/*Mobile View*/}
+                        <div>{showMenu ? (
+                            <HiMenuAlt1 onClick={toggleMenu} 
+                            className="cursor-pointer" 
+                            size={30} />
+                        ) : (
+                            <HiMenuAlt3 onClick={toggleMenu}
+                             className="cursor-pointer" 
+                             size={30} />
+                        )}
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
